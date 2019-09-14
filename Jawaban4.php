0bb6a2e1a930da7254498cd37281ed5d6a6918af
bokri11 <33715347+bokri11@users.noreply.github.com>
2019-09-14 16:15:56 +0000

    Create Jawaban4.php

---
+++ Jawaban4.php
@@ -0,0 +1,13 @@
+
+<?php
+function nama_fungsi(parameter1, parameter2, parameter3){
+    function luas_persegi_panjang($panjang, $lebar){
+        $luas = $panjang * $lebar;
+        echo "Luas Persegi Panjang : " $luas;
+    }
+    luas_persegi_panjang(9, 5);
+}
+
+nama_fungsi(argumen1, argumen2, argumen3);
+
+?>
